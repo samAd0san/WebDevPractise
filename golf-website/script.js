09a406288f47161d5743dfc106964c87b2ad695f
Sohaib samad <sohaibsamad.hala@gmail.com>
2024-09-28 16:47:12 +0530

    Added Dark Effect on Scrolling Improved the nav bar

--- golf-website/script.js
+++ golf-website/script.js
@@ -1,13 +1,25 @@
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.5,
-    height: "110px",
+    height: "90px",
     scrollTrigger: {
-      trigger: "#nav",
-      scroller: "body",
-      // markers:true,
-      start: "top -10%",
-      end: "top -11%",
-      scrub: 1,
+        trigger: "#nav",
+        scroller: "body",
+        // markers:true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1,
     },
-  });
+});
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        // markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2,
+    },
+});
