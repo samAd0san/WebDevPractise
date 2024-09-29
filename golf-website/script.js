f2e204e32109a605c920a14decaeed5b9302b163
Sohaib samad <sohaibsamad.hala@gmail.com>
2024-09-29 18:04:17 +0530

    Added Page 4 Added the CSS stylings Added Transition on Scrolling

--- golf-website/script.js
+++ golf-website/script.js
@@ -99,3 +99,15 @@ gsap.from("#colon2", {
         scrub: 4,
     },
 });
+
+gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+        trigger: "#page4 h1",
+        scroller: "body",
+        // markers:true,
+        start: "top 75%",
+        end: "top 70%",
+        scrub: 3,
+    },
+});
