ac7fc394ca5e35df137fb5bb11b66a9f5b33710d
Sohaib samad <sohaibsamad.hala@gmail.com>
2024-09-29 17:50:54 +0530

    Added Transition on scroll Colon1 and Colon2

--- golf-website/script.js
+++ golf-website/script.js
@@ -74,3 +74,28 @@ gsap.from("#about-us img,#about-us-in", {
     },
 });
 
+// Adding tansitions in colon on scroll
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        // markers:true,
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+    },
+});
+gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        // markers:true,
+        start: "top 55%",
+        end: "top 45%",
+        scrub: 4,
+    },
+});
