7eea33df0fde8af6ccbc755f3b5ebca61b1086fb
Sohaib samad <sohaibsamad.hala@gmail.com>
2024-09-29 17:48:42 +0530

    Added Transition on Scroll About Page

--- golf-website/script.js
+++ golf-website/script.js
@@ -21,16 +21,16 @@ animateCursor(); // Start the animation
 // when we hover to elements of nav bar it should change the cursor
 var h4all = document.querySelectorAll("#nav h4");
 h4all.forEach(function (elem) {
-  elem.addEventListener("mouseenter", function () {
-    crsr.style.scale = 3;
-    crsr.style.border = "1px solid #fff";
-    crsr.style.backgroundColor = "transparent";
-  });
-  elem.addEventListener("mouseleave", function () {
-    crsr.style.scale = 1;
-    crsr.style.border = "0px solid #95C11E";
-    crsr.style.backgroundColor = "#95C11E";
-  });
+    elem.addEventListener("mouseenter", function () {
+        crsr.style.scale = 3;
+        crsr.style.border = "1px solid #fff";
+        crsr.style.backgroundColor = "transparent";
+    });
+    elem.addEventListener("mouseleave", function () {
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #95C11E";
+        crsr.style.backgroundColor = "#95C11E";
+    });
 });
 
 gsap.to("#nav", {
@@ -58,3 +58,19 @@ gsap.to("#main", {
         scrub: 2,
     },
 });
+
+// for adding transition in about page on Scrolling
+gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: "#about-us",
+        scroller: "body",
+        // markers:true,
+        start: "top 70%",
+        end: "top 65%",
+        scrub: 1,
+    },
+});
+
